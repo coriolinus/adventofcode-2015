47676e6bc573479235c33466368fb4891e8e51b8
coriolinus <coriolinus@gmail.com>
2015-12-09 15:11:11 +0100

    Implemented binary to use from command line

--- day3/src/bin.rs
+++ day3/src/bin.rs
@@ -1,1 +1,30 @@
-fn main() {}
+extern crate day3lib;
+use day3lib::follow_santa;
+
+use std::io;
+use std::io::prelude::*;
+
+fn main() {
+    if let Ok(input) = get_input("Enter Santa's path now: ", false) {
+        let cc = follow_santa(input);
+        println!("Houses visited: {}", cc.trail.len());
+    } else {
+        println!("Failed to parse; try again later")
+    }
+}
+
+fn get_input(prompt: &str, wait_for_eof: bool) -> io::Result<String> {
+    print!("{}", prompt);
+
+    // flush stdout explicitly so it shows up at the end of the line
+    try!(io::stdout().flush());
+
+    let mut input = String::new();
+    if wait_for_eof {
+        try!(io::stdin().read_to_string(&mut input));
+    } else {
+        try!(io::stdin().read_line(&mut input));
+    }
+
+    Ok(input) // wrap the output string in Ok to match our signature
+}
