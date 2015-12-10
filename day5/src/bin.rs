5c17ce42220540ba040e585e5abc621dd0bb4b57
coriolinus <coriolinus@gmail.com>
2015-12-10 15:55:44 +0100

    2nd part in and testing correctly.

--- day5/src/bin.rs
+++ day5/src/bin.rs
@@ -3,8 +3,12 @@ use util::get_multiline_input;
 
 extern crate day5lib;
 use day5lib::count_nice;
+use day5lib::count_nice2;
 
 fn main() {
-    let nice = count_nice(&get_multiline_input("Your EOF-terminated lines go here:\n").unwrap());
+    let input = get_multiline_input("Your EOF-terminated lines go here:\n").unwrap();
+    let nice = count_nice(&input);
     println!("Nice: {}", nice);
+    let nice2 = count_nice2(&input);
+    println!("Nice2: {}", nice2);
 }
--- day5/src/lib.rs
+++ day5/src/lib.rs
@@ -60,6 +60,57 @@ pub fn is_nice(input: &str) -> bool {
     has_double && !has_naughty && vowels >= 3
 }
 
+/// Realizing the error of his ways, Santa has switched to a better model of determining whether a
+/// string is naughty or nice. None of the old rules apply, as they are all clearly ridiculous.
+///
+/// Now, a nice string is one with all of the following properties:
+///
+/// - It contains a pair of any two letters that appears at least twice in the string without
+///   overlapping, like `xyxy` (`xy`) or `aabcdefgaa` (`aa`), but not like `aaa` (`aa`, but it
+///   overlaps).
+/// - It contains at least one letter which repeats with exactly one letter between them, like
+///   `xyx`, `abcdefeghi` (`efe`), or even `aaa`.
+fn is_nice2(input: &str) -> bool {
+    let input = input.trim().to_string().to_lowercase();
+    if input.len() == 0 {
+        return false;
+    }
+
+    // simple to iterate through the string and find a pair which repeats with a gap
+    let char_skip_char = input.chars().zip(input.chars().skip(2));
+    let mut rep_skip = false;
+    for (first, second) in char_skip_char {
+        if first == second {
+            rep_skip = true;
+            break;
+        }
+    }
+
+    // break early to skip the expensive stuff, if we contain
+    if !rep_skip {
+        return rep_skip;
+    }
+
+    // now, the hard part: non-overlapping repeated pairs
+    let mut nono_doubles = false;
+    let in_len = input.len();
+    let e_char_pairs = input.chars().zip(input.chars().skip(1)).enumerate();
+    'outer: for (i, (first, second)) in e_char_pairs {
+        if (i + 3) < in_len {
+            let next_char_pairs = input.chars().zip(input.chars().skip(1)).skip(i + 2);
+            for (third, fourth) in next_char_pairs {
+                if first == third && second == fourth {
+                    nono_doubles = true;
+                    break 'outer;
+                }
+            }
+        }
+    }
+
+    rep_skip && nono_doubles
+}
+
+
 fn get_vowels() -> HashSet<char> {
     let mut set = HashSet::new();
     set.insert('a');
@@ -79,7 +130,7 @@ fn get_naughty_pairs() -> HashSet<(char, char)> {
     set
 }
 
-pub fn count_nice(lines: &str) -> i32 {
+pub fn count_nice(lines: &str) -> u32 {
     lines.split("\n").fold(0, |acc, line| {
         acc +
         if is_nice(line) {
@@ -90,9 +141,21 @@ pub fn count_nice(lines: &str) -> i32 {
     })
 }
 
+pub fn count_nice2(lines: &str) -> u32 {
+    lines.split("\n").fold(0, |acc, line| {
+        acc +
+        if is_nice2(line) {
+            1
+        } else {
+            0
+        }
+    })
+}
+
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use super::is_nice;
+    use super::is_nice2;
 
     /// - `ugknbfddgicrmopn` is nice because it has at least three vowels (`u...i...o...`), a double
     ///   letter (`...dd...`), and none of the disallowed substrings.
@@ -108,5 +171,21 @@ mod tests {
         assert!(!is_nice("jchzalrnumimnmhp"));
         assert!(!is_nice("haegwjzuvuyypxyu"));
         assert!(!is_nice("dvszwmarrgswjxmb"));
+    }
+
+    /// - `qjhvhtzxzqqjkmpb` is nice because is has a pair that appears twice (`qj`) and a letter
+    ///   that repeats with exactly one letter between them (`zxz`).
+    /// - `xxyxx` is nice because it has a pair that appears twice and a letter that repeats with
+    ///   one between, even though the letters used by each rule overlap.
+    /// - `uurcxstgmygtbstg` is naughty because it has a pair (`tg`) but no repeat with a single
+    ///   letter between them.
+    /// - `ieodomkazucvgmuy` is naughty because it has a repeating letter with one between (`odo`),
+    ///   but no pair that appears twice.
+    #[test]
+    fn test_examples2() {
+        assert!(is_nice2("qjhvhtzxzqqjkmpb"));
+        assert!(is_nice2("xxyxx"));
+        assert!(!is_nice2("uurcxstgmygtbstg"));
+        assert!(!is_nice2("ieodomkazucvgmuy"));
     }
 }
