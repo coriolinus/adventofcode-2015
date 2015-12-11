0aa102a7f1c2673b7009a15d5b47662d9b050b2b
coriolinus <coriolinus@gmail.com>
2015-12-11 11:56:20 +0100

    Parsing code in place

--- day7/src/lib.rs
+++ day7/src/lib.rs
@@ -1,3 +1,80 @@
-#[test]
-fn it_works() {
+//! # Day 7: Some Assembly Required
+//!
+//! This year, Santa brought little Bobby Tables a set of wires and bitwise logic gates!
+//! Unfortunately, little Bobby is a little under the recommended age range, and he needs help
+//! assembling the circuit.
+//!
+//! Each wire has an identifier (some lowercase letters) and can carry a 16-bit signal (a number
+//! from `0` to `65535`). A signal is provided to each wire by a gate, another wire, or some
+//! specific value. Each wire can only get a signal from one source, but can provide its signal to
+//! multiple destinations. A gate provides no signal until all of its inputs have a signal.
+//!
+//! The included instructions booklet describes how to connect the parts together: `x AND y -> z`
+//! means to connect wires `x` and `y` to an `AND` gate, and then connect its output to wire `z`.
+//!
+//! # For example:
+//!
+//! - `123 -> x` means that the signal `123` is provided to wire `x`.
+//! - `x AND y -> z` means that the bitwise `AND` of wire `x` and wire `y` is provided to wire `z`.
+//! - `p LSHIFT 2 -> q` means that the value from wire `p` is left-shifted by `2` and then provided
+//!   to wire `q`.
+//! - `NOT e -> f` means that the bitwise complement of the value from wire `e` is provided to wire
+//!   `f`.
+//! - Other possible gates include `OR` (bitwise OR) and `RSHIFT` (right-shift). If, for some
+//!   reason, you'd like to emulate the circuit instead, almost all programming languages (for
+//!   example, C, JavaScript, or Python) provide operators for these gates.
+
+#[macro_use]
+extern crate lazy_static;
+
+mod parse;
+use parse::Name;
+use parse::Instruction;
+use parse::parse_instruction;
+
+pub struct Wire {
+    name: Name,
+    value: Option<u16>,
+
+    instruction: Instruction,
+}
+
+impl Wire {
+    pub fn parse(input: &str) -> Option<Wire> {
+        let input = input.trim().to_lowercase();
+        if input.is_empty() {
+            return None;
+        }
+
+        let mut tokens = input.rsplit(' ');
+        let name = tokens.next();
+        if name.is_none() {
+            return None;
+        }
+        let name = Name::new(name.unwrap());
+        if name.is_none() {
+            return None;
+        }
+        let name = name.unwrap();
+
+        let arrow = tokens.next();
+        if arrow.is_none() {
+            return None;
+        }
+        if arrow.unwrap() != "->" {
+            return None;
+        }
+
+        let mut inst: Vec<_> = tokens.collect();
+        // remember, it's backwards because it was a right iterator
+        inst.reverse();
+
+        let inst = parse_instruction(&inst);
+        if inst.is_none() {
+            return None;
+        }
+        let inst = inst.unwrap();
+
+        Some(Wire {name: name, value: None, instruction: inst})
+    }
 }
---
+++ day7/src/parse.rs
@@ -0,0 +1,172 @@
+//! Tools for parsing the given instructions into a data structure mapping them.
+
+
+
+
+use std::collections::HashSet;
+
+
+lazy_static! {
+    static ref LETTERS: HashSet<char> = {
+        let mut letters = HashSet::new();
+        for letter in "abcdefghijklmnopqrstuvwxyz".chars() {
+            letters.insert(letter);
+        }
+        letters
+    };
+
+    static ref NUMBERS: HashSet<char> = {
+        let mut numbers = HashSet::new();
+        for number in "1234567890".chars() {
+            numbers.insert(number);
+        }
+        numbers
+    };
+}
+
+fn is_just_letters(s: &str) -> bool {
+    for c in s.chars() {
+        if !LETTERS.contains(&c) {
+            return false;
+        }
+    }
+    true
+}
+
+fn is_just_numbers(s: &str) -> bool {
+    for c in s.chars() {
+        if !NUMBERS.contains(&c) {
+            return false;
+        }
+    }
+    true
+}
+
+pub enum Instruction {
+    // Binary infix
+    And {
+        x: Evaluable,
+        y: Evaluable,
+    },
+    Or {
+        x: Evaluable,
+        y: Evaluable,
+    },
+    Lshift {
+        x: Evaluable,
+        y: Evaluable,
+    },
+    Rshift {
+        x: Evaluable,
+        y: Evaluable,
+    },
+    // Unary prefix
+    Not {
+        x: Evaluable,
+    },
+    // Nonary (implied)
+    Store {
+        x: Evaluable,
+    },
+}
+
+pub fn parse_instruction(inst: &Vec<&str>) -> Option<Instruction> {
+    match inst.len() {
+        1 => parse_nonary_instruction(inst[0]), // nonary instruction (direct assignment)
+        2 => parse_unary_instruction(inst[0], inst[1]), // unary instruction
+        3 => parse_binary_instruction(inst[0], inst[1], inst[2]), // binary instruction
+        _ => None,
+    }
+}
+
+fn parse_nonary_instruction(x: &str) -> Option<Instruction> {
+    // the only nonary instruction is Store, so that's easy
+    let ev = parse_evaluable(x);
+    if ev.is_none() {
+        None
+    } else {
+        Some(Instruction::Store { x: ev.unwrap() })
+    }
+}
+
+fn parse_unary_instruction(inst: &str, val: &str) -> Option<Instruction> {
+    // the only unary instruction is "Not"
+    if inst != "not" {
+        return None;
+    }
+    let ev = parse_evaluable(val);
+    if ev.is_none() {
+        None
+    } else {
+        Some(Instruction::Not { x: ev.unwrap() })
+    }
+}
+
+fn parse_binary_instruction(x: &str, inst: &str, y: &str) -> Option<Instruction> {
+    // there are four binary instructions: and, or, lshift, rshift.
+    // before we match against them, though, it's cleaner to make sure the other args
+    //   are all evaluable as well.
+
+    let x = parse_evaluable(x);
+    if x.is_none() {
+        return None;
+    }
+    let x = x.unwrap();
+
+    let y = parse_evaluable(y);
+    if y.is_none() {
+        return None;
+    }
+    let y = y.unwrap();
+
+    match inst {
+        "and" => Some(Instruction::And { x: x, y: y }),
+        "or" => Some(Instruction::Or { x: x, y: y }),
+        "lshift" => Some(Instruction::Lshift { x: x, y: y }),
+        "rshift" => Some(Instruction::Rshift { x: x, y: y }),
+        _ => None,
+    }
+}
+
+fn parse_evaluable(ev: &str) -> Option<Evaluable> {
+    if is_just_letters(ev) {
+        let n = Name::new(ev);
+        if n.is_none() {
+            None
+        } else {
+            Some(Evaluable::Name(n.unwrap()))
+        }
+    } else if is_just_numbers(ev) {
+        let n = u16::from_str_radix(ev, 10);
+        if n.is_err() {
+            None
+        } else {
+            Some(Evaluable::Num(n.unwrap()))
+        }
+    } else {
+        None
+    }
+}
+
+enum Evaluable {
+    Num(u16),
+    Name(Name),
+}
+
+pub struct Name {
+    val: String,
+}
+
+impl Name {
+    pub fn new(v: &str) -> Option<Name> {
+        if is_just_letters(v) {
+            Some(Name { val: v.to_string() })
+        } else {
+            None
+        }
+    }
+
+    pub fn get(&self) -> &str {
+        &self.val
+    }
+}
