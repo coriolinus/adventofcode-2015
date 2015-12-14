4e95a8461d77f2662ffe2b28a69572b0e5cde6e6
coriolinus <coriolinus@gmail.com>
2015-12-14 13:05:19 +0100

    Executable builds, plus name the packages correctly

--- day9/src/bin.rs
+++ day9/src/bin.rs
@@ -1,1 +1,11 @@
-fn main() {}
+extern crate util;
+use util::get_multiline_input;
+
+extern crate day9lib;
+use day9lib::Routes;
+
+fn main() {
+    let lines = get_multiline_input("Enter routes, then EOF: ").unwrap();
+    let shortest = Routes::parse_routes(&lines).find_shortest();
+    println!("Shortest route: {:?}", shortest)
+}
