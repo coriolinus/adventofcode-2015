275b28bd195eb262659a76f5e767f4501d4908ac
coriolinus <coriolinus@gmail.com>
2015-12-15 16:23:53 +0100

    Part II

--- day13/src/bin.rs
+++ day13/src/bin.rs
@@ -6,9 +6,20 @@ use day13lib::{parse_neighbors, find_best_ordering, evaluate_ordering};
 
 fn main() {
     let lines = get_multiline_input("cat your input file and pipe it here:\n").unwrap();
-    let (people, rels) = parse_neighbors(&lines);
+    let (mut people, mut rels) = parse_neighbors(&lines);
     let order = find_best_ordering(&people, &rels);
     let (happiness, _) = evaluate_ordering(&order, &rels);
     println!("Best ordering: {:?}", order);
     println!("Total happiness: {}", happiness);
+
+    for person in &people {
+        rels.insert((person.clone(), "Self".to_string()), 0);
+        rels.insert(("Self".to_string(), person.clone()), 0);
+    }
+    people.insert("Self".to_string());
+
+    let order = find_best_ordering(&people, &rels);
+    let (happiness, _) = evaluate_ordering(&order, &rels);
+    println!("Best with-self ordering: {:?}", order);
+    println!("Total with-self happiness: {}", happiness);
 }
