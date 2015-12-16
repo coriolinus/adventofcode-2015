7e060c43c80f3599e8fa3fa61f9a166d308770b4
coriolinus <coriolinus@gmail.com>
2015-12-16 14:09:38 +0100

    Executable runs (part 1)

--- day15/src/bin.rs
+++ day15/src/bin.rs
@@ -1,1 +1,17 @@
-fn main() {}
+extern crate util;
+use util::get_multiline_input;
+
+extern crate day15lib;
+use day15lib::parse_ingredients;
+use day15lib::recipe::Recipe;
+
+fn main() {
+    let lines = get_multiline_input("Ingredients (EOF term'd):").unwrap();
+    let ings = parse_ingredients(&lines);
+    let basic_recipe = Recipe::new(ings);
+    let best_goodness_recipe = basic_recipe.climb_goodness();
+
+    println!("Best recipe: {:?}", best_goodness_recipe);
+    println!(".. with total goodness: {}",
+             best_goodness_recipe.goodness());
+}
