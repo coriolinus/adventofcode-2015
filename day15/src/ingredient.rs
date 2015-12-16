9572577e97ed022a55942d1583d059ccc8121c78
coriolinus <coriolinus@gmail.com>
2015-12-16 10:27:54 +0100

    Ingredient modeled

---
+++ day15/src/ingredient.rs
@@ -0,0 +1,68 @@
+//! A model of a recipe ingredient
+
+use std::str::FromStr;
+use std::collections::HashMap;
+
+extern crate util;
+use self::util::parse::Parser;
+
+pub struct Ingredient {
+    pub name: String,
+    pub capacity: i32,
+    pub durability: i32,
+    pub flavor: i32,
+    pub texture: i32,
+    pub calories: i32,
+}
+
+impl Ingredient {
+    pub fn parse_line(line: &str) -> Option<Ingredient> {
+        let line = line.trim();
+        if line.is_empty() {
+            return None;
+        }
+
+        let parser = Parser::default()
+                         .require_at_least(Some(11))
+                         .require_fewer_than(Some(12))
+                         .fixed_tokens({
+                             let mut h = HashMap::new();
+                             h.insert(1, "capacity".to_string());
+                             h.insert(3, "durability".to_string());
+                             h.insert(5, "flavor".to_string());
+                             h.insert(7, "texture".to_string());
+                             h.insert(9, "calories".to_string());
+                             h
+                         });
+
+        if let Ok(parse_result) = parser.parse(line) {
+            let ref name = parse_result.tokens[0];
+            let mut name = name.clone();
+            name.pop(); // eliminate the trailing colon
+            let ref cap_s = parse_result.tokens[1];
+            let mut cap_s = cap_s.clone();
+            cap_s.pop(); // eliminate trailing commas
+            let ref dur_s = parse_result.tokens[2];
+            let mut dur_s = dur_s.clone();
+            dur_s.pop();
+            let ref fla_s = parse_result.tokens[3];
+            let mut fla_s = fla_s.clone();
+            fla_s.pop();
+            let ref tex_s = parse_result.tokens[4];
+            let mut tex_s = tex_s.clone();
+            tex_s.pop();
+            let ref cal_s = parse_result.tokens[5];
+
+            Some(Ingredient {
+                name: name.clone(),
+                capacity: i32::from_str(&cap_s).unwrap(),
+                durability: i32::from_str(&dur_s).unwrap(),
+                flavor: i32::from_str(&fla_s).unwrap(),
+                texture: i32::from_str(&tex_s).unwrap(),
+                calories: i32::from_str(&cal_s).unwrap(),
+            })
+        } else {
+            None
+        }
+    }
+}
--- day15/src/lib.rs
+++ day15/src/lib.rs
@@ -1,3 +1,28 @@
-#[test]
-fn it_works() {
-}
+//! # Day 15: Science for Hungry People
+//!
+//! Today, you set out on the task of perfecting your milk-dunking cookie recipe. All you have to
+//! do is find the right balance of ingredients.
+//!
+//! Your recipe leaves room for exactly `100` teaspoons of ingredients. You make a list of the
+//! remaining ingredients you could use to finish the recipe (your puzzle input) and their
+//! properties per teaspoon:
+//!
+//! - `capacity` (how well it helps the cookie absorb milk)
+//! - `durability` (how well it keeps the cookie intact when full of milk)
+//! - `flavor` (how tasty it makes the cookie)
+//! - `texture` (how it improves the feel of the cookie)
+//! - `calories` (how many calories it adds to the cookie)
+//!
+//! You can only measure ingredients in whole-teaspoon amounts accurately, and you have to be
+//! accurate so you can reproduce your results in the future. The total score of a cookie can be
+//! found by adding up each of the properties (negative totals become 0) and then multiplying
+//! together everything except calories.
+//!
+//! This program hill-climbs to a local maximum and hopes for the best.
+
+
+
+
+pub mod ingredient;
+
+const TOTAL_INGREDIENTS: u32 = 100;
