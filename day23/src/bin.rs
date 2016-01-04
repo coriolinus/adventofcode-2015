0678456ee6c6804dc6bf560a4bff4ccf3aae36ca
coriolinus <coriolinus@gmail.com>
2016-01-04 18:39:08 +0100

    Implement executable and solve part 1

--- day23/src/bin.rs
+++ day23/src/bin.rs
@@ -1,1 +1,17 @@
-fn main() {}
+extern crate util;
+use util::get_multiline_input;
+
+extern crate day23lib;
+use day23lib::{Instruction, CPU, Register};
+
+fn main() {
+    let program = get_multiline_input("Input program, EOF-terminated:").unwrap();
+    let insts = Instruction::parse_lines(&program);
+    if let Ok(insts) = insts {
+        let mut cpu = CPU::from_instructions(insts);
+        cpu.run();
+        println!("Terminating with register B = '{}'", cpu.get(Register::B));
+    } else {
+        println!("Could not parse your instructions, sorry;")
+    }
+}
