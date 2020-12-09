d8b6cd81479d114120108b1e6587ab5738c859cc
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2020-12-09 16:00:38 +0100

    refactor day 09 to modern style
    It was pleasant to see how little this code actually needed to change:
    while `main.rs` needed rewriting, and a little boilerplate needed
    adding, the vast majority of functional problem-solving code only needed
    very minor touch-ups. The main improvements here are just in cleaning
    up the parsing.

--- day09/src/lib.rs
+++ day09/src/lib.rs
@@ -7,97 +7,84 @@
 //! locations he wants, but he must visit each location exactly once. What is the shortest distance
 //! he can travel to achieve this?
 
+use aoc2015::parse;
+use permutohedron::heap_recursive;
 use std::cmp::Ordering;
 use std::collections::{HashMap, HashSet};
+use std::path::Path;
+use thiserror::Error;
 
-use util::parse::Parser;
+#[derive(Debug)]
+pub struct Route {
+    pub stops: Vec<String>,
+    pub dist: u32,
+}
 
-use permutohedron::heap_recursive;
+#[derive(Clone, Debug, parse_display::FromStr, parse_display::Display)]
+#[display("{from} to {to} = {distance}")]
+struct Edge {
+    from: String,
+    to: String,
+    distance: u32,
+}
 
 // we're going to do this the quick, dumb way.
-type DistMap = HashMap<(String, String), usize>;
+type DistMap = HashMap<(String, String), u32>;
 
 pub struct Routes {
     dist_map: DistMap,
     places: HashSet<String>,
 }
 
+impl std::iter::FromIterator<Edge> for Routes {
+    fn from_iter<T: IntoIterator<Item = Edge>>(iter: T) -> Self {
+        let iter = iter.into_iter();
+        let (min_contents, _) = iter.size_hint();
+
+        let mut dist_map = DistMap::with_capacity(min_contents);
+        let mut places = HashSet::with_capacity(min_contents);
+
+        for Edge { from, to, distance } in iter {
+            dist_map.insert((from.clone(), to.clone()), distance);
+            dist_map.insert((to.clone(), from.clone()), distance);
+            places.insert(from);
+            places.insert(to);
+        }
+
+        Routes { dist_map, places }
+    }
+}
+
 impl Routes {
-    fn new() -> Routes {
-        Routes {
-            dist_map: DistMap::new(),
-            places: HashSet::new(),
-        }
-    }
+    fn find_extreme(&self, order: Ordering, default_dist: u32) -> Route {
+        let mut places: Vec<_> = self.places.iter().collect();
 
-    pub fn parse_routes(lines: &str) -> Routes {
-        // create parser
-        let parser = Parser::default()
-            .force_lowercase(false)
-            .require_at_least(Some(5))
-            .require_fewer_than(Some(6))
-            .fixed_tokens({
-                let mut h = HashMap::new();
-                h.insert(1, "to".to_string());
-                h.insert(3, "=".to_string());
-                h
-            });
-
-        let mut o = Routes::new();
-
-        for line in lines.split('\n') {
-            let line = line.trim();
-            if line.is_empty() {
-                continue;
-            }
-
-            if let Ok(v) = parser.parse(line) {
-                let ref from = v.tokens[0];
-                let ref to = v.tokens[1];
-                let ref dist = v.tokens[2];
-
-                if let Ok(dist) = usize::from_str_radix(&dist, 10) {
-                    o.add(from.clone(), to.clone(), dist);
-                }
-            }
-        }
-
-        o
-    }
-
-    pub fn add(&mut self, from: String, to: String, dist: usize) {
-        self.dist_map.insert((from.clone(), to.clone()), dist);
-        self.dist_map.insert((to.clone(), from.clone()), dist);
-        self.places.insert(from.clone());
-        self.places.insert(to.clone());
-    }
-
-    pub fn find_extreme(&self, order: Ordering, default_dist: usize) -> Route {
-        let mut places = self.places.iter().collect::<Vec<_>>();
-
-        let mut ret = Route {
+        let mut route = Route {
             stops: Vec::new(),
             dist: default_dist,
         };
-        heap_recursive(&mut places, |ordering| {
-            // ordering = [&String]
-            let this_dist: usize = ordering
-                .windows(2)
-                .map(|window| (window[0], window[1]))
-                .map(|(from, to)| self.dist_map.get(&(from.clone(), to.clone())).unwrap())
-                .fold(0, std::ops::Add::add);
 
-            if this_dist.cmp(&ret.dist) == order {
-                ret.stops = ordering.iter().map(|&s| s.clone()).collect::<Vec<_>>();
-                ret.dist = this_dist;
+        heap_recursive(&mut places, |ordering| {
+            let this_dist: u32 = ordering
+                .windows(2)
+                .map(|window| {
+                    let from = window[0];
+                    let to = window[1];
+                    self.dist_map.get(&(from.clone(), to.clone())).unwrap()
+                })
+                .sum();
+
+            if this_dist.cmp(&route.dist) == order {
+                route.stops = ordering.iter().map(|&s| s.clone()).collect::<Vec<_>>();
+                route.dist = this_dist;
             }
         });
 
-        ret
+        route
     }
 
     pub fn find_shortest(&self) -> Route {
-        self.find_extreme(Ordering::Less, usize::max_value())
+        self.find_extreme(Ordering::Less, !0)
     }
 
     pub fn find_longest(&self) -> Route {
@@ -105,15 +92,29 @@ impl Routes {
     }
 }
 
-#[derive(Debug)]
-pub struct Route {
-    pub stops: Vec<String>,
-    pub dist: usize,
+pub fn part1(input: &Path) -> Result<(), Error> {
+    let routes: Routes = parse(input)?.collect();
+    let shortest = routes.find_shortest();
+    println!("shortest route length: {}", shortest.dist);
+    Ok(())
+}
+
+pub fn part2(input: &Path) -> Result<(), Error> {
+    let routes: Routes = parse(input)?.collect();
+    let longest = routes.find_longest();
+    println!("longest route length: {}", longest.dist);
+    Ok(())
+}
+
+#[derive(Debug, Error)]
+pub enum Error {
+    #[error(transparent)]
+    Io(#[from] std::io::Error),
 }
 
 #[cfg(test)]
 mod test {
-    use super::Routes;
+    use super::*;
 
     /// For example, given the following distances:
     ///
@@ -134,12 +135,15 @@ mod test {
     #[test]
     fn test_example_shortest() {
         let lines = "London to Dublin = 464\nLondon to Belfast = 518\nDublin to Belfast = 141";
-        let mut expected_route = vec!["London", "Dublin", "Belfast"]
+        let mut expected_route = ["London", "Dublin", "Belfast"]
             .iter()
             .map(|s| s.to_string())
             .collect::<Vec<_>>();
 
-        let routes = Routes::parse_routes(lines);
+        let routes: Routes = lines
+            .split('\n')
+            .map(|line| line.parse::<Edge>().unwrap())
+            .collect();
         let shortest = routes.find_shortest();
 
         println!("Shortest route: {:?}", shortest);
@@ -163,12 +167,15 @@ mod test {
     #[test]
     fn test_example_longest() {
         let lines = "London to Dublin = 464\nLondon to Belfast = 518\nDublin to Belfast = 141";
-        let mut expected_route = vec!["Dublin", "London", "Belfast"]
+        let mut expected_route = ["Dublin", "London", "Belfast"]
             .iter()
             .map(|s| s.to_string())
             .collect::<Vec<_>>();
 
-        let routes = Routes::parse_routes(lines);
+        let routes: Routes = lines
+            .split('\n')
+            .map(|line| line.parse::<Edge>().unwrap())
+            .collect();
         let longest = routes.find_longest();
 
         println!("Longest route: {:?}", longest);
--- day09/src/main.rs
+++ day09/src/main.rs
@@ -1,12 +1,52 @@
-use util::get_multiline_input;
+use aoc2015::{config::Config, website::get_input};
+use day09::{part1, part2};
 
-use day09::Routes;
+use color_eyre::eyre::Result;
+use std::path::PathBuf;
+use structopt::StructOpt;
 
-fn main() {
-    let lines = get_multiline_input("Enter routes, then EOF: ").unwrap();
-    let routes = Routes::parse_routes(&lines);
-    let shortest = routes.find_shortest();
-    println!("Shortest route: {:?}", shortest);
-    let longest = routes.find_longest();
-    println!("Longest route: {:?}", longest);
+const DAY: u8 = 9;
+
+#[derive(StructOpt, Debug)]
+struct RunArgs {
+    /// input file
+    #[structopt(long, parse(from_os_str))]
+    input: Option<PathBuf>,
+
+    /// skip part 1
+    #[structopt(long = "no-part1")]
+    no_part1: bool,
+
+    /// run part 2
+    #[structopt(long)]
+    part2: bool,
+}
+
+impl RunArgs {
+    fn input(&self) -> Result<PathBuf> {
+        match self.input {
+            None => {
+                let config = Config::load()?;
+                // this does nothing if the input file already exists, but
+                // simplifies the workflow after cloning the repo on a new computer
+                get_input(&config, DAY)?;
+                Ok(config.input_for(DAY))
+            }
+            Some(ref path) => Ok(path.clone()),
+        }
+    }
+}
+
+fn main() -> Result<()> {
+    color_eyre::install()?;
+    let args = RunArgs::from_args();
+    let input_path = args.input()?;
+
+    if !args.no_part1 {
+        part1(&input_path)?;
+    }
+    if args.part2 {
+        part2(&input_path)?;
+    }
+    Ok(())
 }
