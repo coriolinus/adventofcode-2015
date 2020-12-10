4198b9c7999d79770b4062faa4015491a7f99fa6
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2020-12-10 17:16:07 +0100

    implement day 12 in rust
    I don't know what I was on back in the day when I claimed that Rust
    didn't have good support for handling arbitrary JSON; I think the
    solution function here is actually shorter than the two solution
    functions as they were in Python! Clearly, what I should have said
    was that at that time, I didn't know what to do with JSON in Rust.
    
    Also, this was weirdly easy, largely because Rust has great JSON
    support. Not what I'd expect from day 12 this year!

---
+++ day12/src/lib.rs
@@ -0,0 +1,53 @@
+use aoc2015::parse;
+use std::path::Path;
+use thiserror::Error;
+
+/// sum up integers in a json value
+///
+/// we can represent these as integers, as there are no decimal points in the input
+fn sum_of_numbers_in(
+    value: &serde_json::Value,
+    filter_objects: &dyn Fn(&serde_json::Map<String, serde_json::Value>) -> bool,
+) -> i64 {
+    let sum_inner = |value: &serde_json::Value| sum_of_numbers_in(value, filter_objects);
+
+    match value {
+        serde_json::Value::Number(n) => n.as_i64().unwrap_or_default(),
+        serde_json::Value::Array(values) => values.iter().map(sum_inner).sum(),
+        serde_json::Value::Object(object) => {
+            if filter_objects(object) {
+                object.values().map(sum_inner).sum()
+            } else {
+                0
+            }
+        }
+        _ => 0,
+    }
+}
+
+pub fn part1(input: &Path) -> Result<(), Error> {
+    let numbers_in: i64 = parse::<serde_json::Value>(input)?
+        .map(|value| sum_of_numbers_in(&value, &|_| true))
+        .sum();
+    println!("numbers in the input: {}", numbers_in);
+    Ok(())
+}
+
+pub fn part2(input: &Path) -> Result<(), Error> {
+    let numbers_in: i64 = parse::<serde_json::Value>(input)?
+        .map(|value| {
+            sum_of_numbers_in(&value, &|obj| {
+                !obj.values()
+                    .any(|value| value.as_str().map(|s| s == "red").unwrap_or_default())
+            })
+        })
+        .sum();
+    println!("non-red numbers in the input: {}", numbers_in);
+    Ok(())
+}
+
+#[derive(Debug, Error)]
+pub enum Error {
+    #[error(transparent)]
+    Io(#[from] std::io::Error),
+}
